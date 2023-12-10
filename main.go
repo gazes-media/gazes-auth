bdda1743165b0fdade6294c86b6f5cc735b948a0
garder500 <jeremy27.clara22@gmail.com>
2023-12-10 15:59:18 +0100

    Init of the Database in main.go

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"gazes-auth/database"
+	"gazes-auth/routes"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
+func init() {
+	database.Init()
+}
+
 func main() {
 	r := mux.NewRouter()
-	http.Handle("/", r)
+	r.PathPrefix("/login").HandlerFunc(routes.LoginHandler).Methods("POST")
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: r,
 	}
+
 	server.ListenAndServe()
 }
