fdcc58c2e48caa01bb935b32105a4e15c4a81300
garder500 <jeremy27.clara22@gmail.com>
2023-12-12 01:17:04 +0100

    Adding JWT

--- auth/jwt.go
+++ auth/jwt.go
@@ -8,31 +8,34 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-// JWT is a wrapper around the jwt.Token type
-
+// UserClaim is the expected format of the JWT token, it contains the user ID and username
 type UserClaim struct {
 	jwt.RegisteredClaims
-	Id       int    `json:"id"`
-	Username string `json:"username"`
+	Id       uint   `json:"id"`
+	Email string `json:"email"`
 }
 
+// UserLogin is the expected format of the request body when logging in
 type UserLogin struct {
-	Username string `json:"username"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
+// UserRegister is the expected format of the request body when registering
 type UserRegister struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
+// SecretToken is the secret used to sign the JWT token, (it should be stored in an environment variable) #TODO: store in env var
 var SecretToken = []byte("7c8mjYGG5H6VXyf6Zxqq6m69a2XNnPVC")
 
+// Sign creates a JWT token from a user object
 func Sign(user database.User) (string, error) {
 	claims := UserClaim{
-		Id:       int(user.ID),
-		Username: user.Username,
+		Id:       user.ID,
+		Email: user.Email,
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(7 * 24 * time.Hour)),
 			Subject:   "user",
@@ -44,6 +47,7 @@ func Sign(user database.User) (string, error) {
 	return token.SignedString(SecretToken)
 }
 
+// Verify checks if a JWT token is valid and returns the user Claim Object
 func Verify(tokenString string) (*UserClaim, error) {
 	claims := &UserClaim{}
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
