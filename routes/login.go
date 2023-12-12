874f591956f6eaabcbeb507376829016db0dfff9
garder500 <jeremy27.clara22@gmail.com>
2023-12-12 01:18:10 +0100

    Login Route working, (need to be checked)

--- routes/login.go
+++ routes/login.go
@@ -4,8 +4,13 @@ import (
 	"encoding/json"
 	"gazes-auth/auth"
 	"net/http"
+	"gazes-auth/database"
 )
 
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -18,4 +23,34 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// check if user exists
+	u := database.User{Email: user.Email}
+	err = u.GetByEmail()
+	if err != nil {
+		jsonErr := ErrorResponse{Error: "User does not exist"}
+		jsonErr.Write(w, http.StatusUnauthorized)
+		return
+	}
+
+	// check if password is correct
+	if !auth.ComparePassword(u.Password, user.Password) {
+		jsonErr := ErrorResponse{Error: "Invalid password"}
+		jsonErr.Write(w, http.StatusUnauthorized)
+		return
+	}
+
+	// create JWT token
+	token, err := auth.Sign(u)
+	if err != nil {
+		jsonErr := ErrorResponse{Error: "Error creating token"}
+		jsonErr.Write(w, http.StatusInternalServerError)
+		return
+	}
+
+	// send token in response
+	response := LoginResponse{Token: token}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+	
 }
