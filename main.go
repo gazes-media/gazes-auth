c1e633d880ae246137d6f325b17fad121e22bc6b
garder500 <jeremy27.clara22@gmail.com>
2023-12-12 01:17:29 +0100

    Adding routes & Documentations

--- main.go
+++ main.go
@@ -8,13 +8,21 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// init function is called before main function, so we can initialize our database connection here
 func init() {
 	database.Init()
 }
 
 func main() {
+	// Creating a new Mux Router
 	r := mux.NewRouter()
+	// Handling Login Route
 	r.PathPrefix("/login").HandlerFunc(routes.LoginHandler).Methods("POST")
+	// Handling Register Route
+	r.PathPrefix("/register").HandlerFunc(routes.RegisterHandler).Methods("POST")
+	// Handling UserMe Route
+	r.PathPrefix("/user/@me").HandlerFunc(routes.UserMeHandler).Methods("GET")
+	// Defining the Server
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: r,
